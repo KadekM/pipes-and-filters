2874a0f55975bfa5986e603c2112edd0d4faad68
visox <viso.laci@gmail.com>
2014-08-24 14:02:44 +0200

    forgot to add new files

--- MainController.js
+++ MainController.js
@@ -1,3 +1,38 @@
 /**
  * Created by Michal on 24. 8. 2014.
  */
+(function() {
+
+    var app = angular.module("webAnalyzer");
+
+    var MainController = function($scope/*, $interval, $location*/) {
+
+        $scope.testVariable = "it is alive !!!";
+       /* var decrementCountdown = function(){
+            $scope.countdown -= 1;
+            if($scope.countdown < 1){
+                $scope.search($scope.username);
+            }
+        };
+
+        var countdownInterval = null;
+        var startCountdown = function(){
+            countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+        };
+
+        $scope.search = function(username) {
+            if(countdownInterval)    {
+                $interval.cancel(countdownInterval);
+                $scope.countdown = null;
+            }
+            $location.path("/user/" + username);
+        };
+
+        $scope.username = "angular";
+        $scope.countdown = 5;
+        startCountdown();*/
+    };
+
+    app.controller("MainController", MainController);
+
+}());
--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 /**
  * Created by Michal on 24. 8. 2014.
  */
+(function(){
+
+    var app = angular.module("webAnalyzer", ["ngRoute"]);
+
+    app.config(function($routeProvider){
+        $routeProvider
+            .when("/main", { // todo later
+                templateUrl: "main.html",
+                controller: "MainController"
+            })
+            .otherwise({redirectTo:"/main"});
+});
+
+}());
