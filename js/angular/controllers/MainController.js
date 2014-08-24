7dad3c2998ac12291f9c074a135d1a335abb439d
Marek Kadek <kadek.marek@gmail.com>
2014-08-24 19:12:31 +0200

    Cleanup

--- js/angular/controllers/MainController.js
+++ js/angular/controllers/MainController.js
@@ -7,32 +7,9 @@
         $scope.testVariable = "it is alive !!!";
 
         $scope.executeAnalyze = function(term) {
-
             $location.path("/term/" + term);
         };
-       /* var decrementCountdown = function(){
-            $scope.countdown -= 1;
-            if($scope.countdown < 1){
-                $scope.search($scope.username);
-            }
-        };
 
-        var countdownInterval = null;
-        var startCountdown = function(){
-            countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
-        };
-
-        $scope.search = function(username) {
-            if(countdownInterval)    {
-                $interval.cancel(countdownInterval);
-                $scope.countdown = null;
-            }
-            $location.path("/user/" + username);
-        };
-
-        $scope.username = "angular";
-        $scope.countdown = 5;
-        startCountdown();*/
     };
 
     app.controller("MainController", MainController);
