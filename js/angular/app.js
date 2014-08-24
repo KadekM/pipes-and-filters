88d05cf56978922d5e03ad0dbc473c39493abb63
Marek Kadek <kadek.marek@gmail.com>
2014-08-24 20:35:49 +0200

    Added start of d3

--- js/angular/app.js
+++ js/angular/app.js
@@ -1,20 +1,17 @@
-(function() {
+(function () {
+    'use strict';
 
-    var app = angular.module("webAnalyzer", ["ngRoute"]);
-
-    app.config(function($routeProvider){
-        $routeProvider
-            .when("/main", { // todo later
-                templateUrl: "main.html",
-                controller: "MainController"
-            })
-            .when("/term/:term", {
-                templateUrl: "term.html",
-                controller: "TermController"
-            })
-            .otherwise({redirectTo:"/main"});
-});
-
-
-
+    angular.module("webAnalyzer", ["ngRoute"])
+        .config(function ($routeProvider) {
+            $routeProvider
+                .when("/main", { // todo later
+                    templateUrl: "main.html",
+                    controller: "MainController"
+                })
+                .when("/term/:term", {
+                    templateUrl: "term.html",
+                    controller: "TermController"
+                })
+                .otherwise({redirectTo: "/main"});
+        });
 }());
