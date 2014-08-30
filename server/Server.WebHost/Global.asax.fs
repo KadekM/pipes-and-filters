4e16b5b0bc78407fa15a4a3261141ea5c2060ed5
Marek Kadek <kadek.marek@gmail.com>
2014-08-30 13:06:01 +0200

    First server draft

---
+++ server/Server.WebHost/Global.asax.fs
@@ -0,0 +1,32 @@
+namespace Server.WebHost
+
+open System
+open System.Web.Http
+open System.Net.Http.Headers
+
+type HttpRoute = {
+    controller : string
+    id : RouteParameter }
+
+type Global() =
+    inherit System.Web.HttpApplication() 
+
+    static member RegisterWebApi(config: HttpConfiguration) =
+        // Configure routing
+        config.MapHttpAttributeRoutes()
+        config.Routes.MapHttpRoute(
+            "DefaultApi", // Route name
+            "{controller}/{id}", // URL with parameters
+            { controller = "{controller}"; id = RouteParameter.Optional } // Parameter defaults
+        ) |> ignore
+
+        // Configure serialization
+        config.Formatters.XmlFormatter.UseXmlSerializer <- false
+        config.Formatters.JsonFormatter.SupportedMediaTypes.Add(MediaTypeHeaderValue("text/html") );
+        config.Formatters.JsonFormatter.UseDataContractJsonSerializer <- true
+        config.Formatters.JsonFormatter.SerializerSettings.ContractResolver <- Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver()
+
+        // Additional Web API settings
+
+    member x.Application_Start() =
+        GlobalConfiguration.Configure(Action<_> Global.RegisterWebApi)
