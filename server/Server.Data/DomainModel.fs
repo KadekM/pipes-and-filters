fb2de4561f5024fae9a5493176df45de74e4b7da
Marek Kadek <kadek.marek@gmail.com>
2014-08-31 17:08:23 +0200

    Cleaned up

--- server/Server.Data/DomainModel.fs
+++ server/Server.Data/DomainModel.fs
@@ -18,23 +18,6 @@ module Analysis =
 
     let Task id request = {Id = id; Request = request; Data = [||]}
 
-    //
-
     let CreateTask request = Task (Guid.NewGuid()) request
 
     let AsTaskInfoResponse task = TaskInfoResponse task.Id <| Link "TODO" ("/analysis/"+task.Id.ToString())
-
-(*
-type IAnalysisTasks =
-    inherit seq<AnalysisTask> 
-
-type AnalysisTasksInMemory(analysisTasks: seq<AnalysisTask>) =
-    interface IAnalysisTasks with
-        member this.GetEnumerator() =
-            analysisTasks.GetEnumerator()
-        member this.GetEnumerator() =
-            (this :> seq<AnalysisTask>).GetEnumerator() :> IEnumerable<>
-
-let ToAnalysisTasks tasks = 
-    AnalysisTasksInMemory(tasks)
-    *)
