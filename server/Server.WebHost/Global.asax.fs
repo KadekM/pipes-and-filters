e50af6d589439826c3ee44f7a1bde2cde6713274
Marek Kadek <kadek.marek@gmail.com>
2014-08-31 15:41:16 +0200

    Decoupled implementation of agent from concurrent bag

--- server/Server.WebHost/Global.asax.fs
+++ server/Server.WebHost/Global.asax.fs
@@ -15,11 +15,15 @@ type Global() =
     member this.Application_Start() =
         let tasks = ConcurrentBag<AnalysisTask>()
         let maxJobs = 5
+
+        let tasksSubject = new Subjects.Subject<AnalysisTask>()
+        tasksSubject.Subscribe tasks.Add |> ignore
+
         let agent = new Agent<AnalysisTask>(fun inbox ->
             let rec loop() = 
                 async {
-                    let! cmd = inbox.Receive()
-                    tasks.Add(cmd)
+                    let! task = inbox.Receive()
+                    tasksSubject.OnNext task
                   //  let handle = Handle maxJobs tasks
                   //todo <--- --->
                     return! loop() }
