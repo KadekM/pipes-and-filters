71eb2b239818cd8acf462c278c3a622ea5a49ab4
Marek Kadek <kadek.marek@gmail.com>
2014-09-25 16:36:19 +0200

    Renamed Server.Data to correct name. Added Testing project

---
+++ server/Server.WebHost.Tests/Test.fs
@@ -0,0 +1,13 @@
+﻿module Test
+
+open NUnit.Framework
+open Server.WebHost
+
+
+[<TestFixture>]
+type TestClass() = 
+
+    [<Test>]
+    member this.When2IsAddedTo2Expect4() = 
+        let gl = new Global()
+        ()
